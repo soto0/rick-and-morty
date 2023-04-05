010e390384b4d7eaadd5352497542a5a4b375edb
Nik <kaden0065@gmail.com>
2023-04-06 00:21:37 +0300

    add loader

--- src/components/Characters/CharactersList/CharactersList.tsx
+++ src/components/Characters/CharactersList/CharactersList.tsx
@@ -1,18 +1,21 @@
 import { FC } from 'react';
 import styles from './CharactersList.module.scss';
 import { charactersAPI } from "@/services/Characters";
-import CharacterCard from "../CharacterCard/CharacterCard";
 import { ICharacters } from "@/models/ICharacters";
+import Loader from "@/components/Loader/Loader";
+import CharacterCard from "../CharacterCard/CharacterCard";
 
 const CharactersList: FC = () => {
-    const { data: characters, error, isLoading } = charactersAPI.useGetCharactersQuery();
+    const { data: characters, error, isLoading, isSuccess } = charactersAPI.useGetCharactersQuery();
 
     return (
         <section className={styles.characters__list}>
             <div className="container">
                 <h3 className={styles.title}>Characters</h3>
                 <div className={styles.characters__block}>
-                    {
+                    {error && <h1>da</h1>}
+                    {isLoading && <Loader />}
+                    {isSuccess &&
                         characters && characters.map((character: ICharacters) => {
                             return (
                                 <CharacterCard
---
+++ src/components/Loader/Loader.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react';
+import Image from "next/image";
+import LoaderGif from './../../icons/Loading/loading.gif';
+
+const Loader: FC = () => {
+    return (
+        <Image src={LoaderGif} width={250} height={200} alt="loader" className="loader" />
+    );
+};
+
+export default Loader;
