052e60670023e1c006344452bfd6a8af36c19eea
Nik <kaden0065@gmail.com>
2023-04-05 18:14:13 +0300

    change category blocks

--- src/components/CategoryCards/CategoryCards.tsx
+++ src/components/CategoryCards/CategoryCards.tsx
@@ -1,14 +1,28 @@
 import { FC } from 'react';
+import Link from "next/link";
+import Image from "next/image";
 import styles from './CategoryCards.module.scss';
+import CharactersIcon from './../../icons/Categories/character.png';
+import LocationsIcon from './../../icons/Categories/location.png';
+import EpisodesIcon from './../../icons/Categories/episode.png';
 
 const CategoryCards: FC = () => {
     return (
         <section className={styles.categories}>
             <div className="container">
                 <div className={styles.categories__block}>
-                    <div className={styles.category__block}>
+                    <Link href="/Characters" className={styles.category__block}>
+                        <Image src={CharactersIcon} alt="characters" className={styles.category__icon} />
                         <h3 className={styles.title}>Characters</h3>
-                    </div>
+                    </Link>
+                    <Link href="/Locations" className={styles.category__block}>
+                        <Image src={LocationsIcon} alt="characters" className={styles.category__icon} />
+                        <h3 className={styles.title}>Locations</h3>
+                    </Link>
+                    <Link href="/Episodes" className={styles.category__block}>
+                        <Image src={EpisodesIcon} alt="characters" className={styles.category__icon} />
+                        <h3 className={styles.title}>Episodes</h3>
+                    </Link>
                 </div>
             </div>
         </section>
