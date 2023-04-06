fec837b1779760bf6e508941c898050f24d5541e
Nik <kaden0065@gmail.com>
2023-04-06 23:56:44 +0300

    added loading and error for character page

--- src/pages/Character/[id].tsx
+++ src/pages/Character/[id].tsx
@@ -5,6 +5,8 @@ import Image from "next/image";
 import Link from "next/link";
 import Layout from "@/layout/Layout";
 import Status from "@/components/Status/Status";
+import Loader from "@/components/Loader/Loader";
+import Error from "@/components/Error/Error";
 import { charactersAPI } from "@/services/Characters";
 import styles from './Character.module.scss';
 
@@ -24,25 +26,33 @@ const Character: FC = () => {
                 <title>{character && character.name} | Rick And Morty</title>
             </Head>
             <Layout>
-                {character && isSuccess && <>
-                    <section className={styles.character__top}>
-                        <div className="container">
-                            <Image className={styles.image} src={character.image} width={300} height={300} alt="icon" />
-                            <h3 className={styles.name}>{character.name}</h3>
-                            <Status Status={character.status} Species={character.species} />
-                        </div>
-                    </section>
-                    <section className={styles.character__bottom}>
-                        <div className="container">
-                            <div className={styles.info}>
-                                <h4 className={styles.title}>Origin</h4>
-                                <Link href={"/Location/" + character.origin.url} className={styles.info__text}>{character.origin.name}</Link>
-                                <h4 className={styles.title}>Last Location</h4>
-                                <Link href={"/Location/" + character.location.url} className={styles.info__text}>{character.location.name}</Link>
+                {error && <Error />}
+                {isLoading && <Loader />}
+                {
+                    character && isSuccess && <>
+                        <section className={styles.character__top}>
+                            <div className="container">
+                                <Image className={styles.image} src={character.image} width={300} height={300} alt="icon" />
+                                <h3 className={styles.name}>{character.name}</h3>
+                                <Status Status={character.status} Species={character.species} />
                             </div>
-                        </div>
-                    </section>
-                </>}
+                        </section>
+                        <section className={styles.character__bottom}>
+                            <div className="container">
+                                <div className={styles.info}>
+                                    <div className={styles.info__block}>
+                                        <h4 className={styles.title}>Origin</h4>
+                                        <Link href={"/Location/" + character.origin.url.slice(41)} className={styles.info__text}>{character.origin.name}</Link>
+                                    </div>
+                                    <div className={styles.info__block}>
+                                        <h4 className={styles.title}>Last Location</h4>
+                                        <Link href={"/Location/" + character.location.url.slice(41)} className={styles.info__text}>{character.location.name}</Link>
+                                    </div>
+                                </div>
+                            </div>
+                        </section>
+                    </>
+                }
             </Layout>
         </>
     );
